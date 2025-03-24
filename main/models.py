8bbc3363a2d791d0765e30c455bcb421363b33b1
giri-saurabh-dev <thisismydeveloperaccount666>
2025-03-24 21:11:08 +0545

    working on css

--- main/models.py
+++ main/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Skill():
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+        
+    def __str__(self):
+        return self.title
+
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -8,3 +16,4 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
